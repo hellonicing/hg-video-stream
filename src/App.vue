c72fd359d61f193f9e6d7e0bef32bdba10ce90a0
unknown <you@example.com>
2023-02-11 14:08:53 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import NavBar from "./components/NavBar.vue";
+  import Tabs from "./components/Tabs.vue";
+  import Category from "./components/Category.vue";
+</script>
+
+<template>
+  <div class="App">
+    <NavBar> </NavBar>
+    <Tabs />
+    <h2>热门</h2>
+    <Category />
+    <h2>直播</h2>
+    <Category />
+    <h2>推荐</h2>
+    <Category />
+  </div>
+</template>
+
+<style scoped></style>
